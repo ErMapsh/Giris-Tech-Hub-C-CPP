cd9e595dc0ec8067798ac16022247151173122c3
Mahesh <maheshmestri73@gmail.com>
2022-12-04 08:44:15 +0530

    .

--- Problems/3Dec/2/Lecture/4MCQBySir.c
+++ Problems/3Dec/2/Lecture/4MCQBySir.c
@@ -5,10 +5,10 @@ int main()
     /*
         so lets find 32 bit address = 100000
         but negative interger on right hand side is undefined bahivour in the C language for right shift.
-        
+
         32 = 100000
-           = 100000 --> here -1 means we just removing number of first 
-           = 00000 => thats why 0
+           = 100000 
+        
     */
 
     printf("%d\n", 32 >> -1);
@@ -19,5 +19,7 @@ int main()
            = 010000 --> thats why its 16
     */
     printf("%d\n", 32 >> 1);
+
+    printf("%d\n", 48 >> -1);
     return 0;
 }
