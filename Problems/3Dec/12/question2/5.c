6506914edb925b1fd57f8bc6777b889fb8785363
Mahesh <maheshmestri73@gmail.com>
2022-12-13 08:56:25 +0530

    .

--- Problems/3Dec/12/question2/5.c
+++ Problems/3Dec/12/question2/5.c
@@ -26,13 +26,14 @@ void Display(int a[3][3])
 }
 
 int Match(int a[3][3], int b[3][3])
-{   
+{
     int flag = 0;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            if(a[i][j] != b[i][j]){
+            if (a[i][j] != b[i][j])
+            {
                 flag = 1;
                 break;
             }
